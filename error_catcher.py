7fe00bf42d515329a019e71cc6d7ea91b4dd08c7
Kalyan Sriram <coder.kalyan@gmail.com>
2018-08-12 17:49:21 -0700

    Started cleanup of linter.

--- error_catcher.py
+++ error_catcher.py
@@ -2,6 +2,27 @@ import sys, os
 import importlib
 import flake8.api.legacy
 from io import StringIO
+import re
+
+class Errors:
+    F402 = "F402"
+    F403 = "F403"
+    F404 = "F404"
+    F405 = "F405"
+    F406 = "F406"
+    F407 = "F407"
+    F8 = "F8"
+    F901 = "F901"
+    E999 = "E999"
+
+    supported = [F402, F403, F404, F405, F406, F407, F8, F901, E999]
+    msg = {
+            F403: "Package not installed or is unavailable.",
+            F405: "{} is undefined."
+        }
+
+get_error = re.compile("[FE]\d{1,3}")
+get_loc = re.compile("\w+.py:\d+:\d+:")
 
 def get_lint(file):
     # Temporarily redirect stdout to the lintstdout variable to get flake8 output
@@ -23,6 +44,7 @@ def get_lint(file):
     verified_packages = []
     sys.path.append("/".join(file.split("/")[:-1]))
     for i in lint.split("\n"):
+        """print(i)
         try:
             if any(error in i for error in ["F402",
                                             "F403",
@@ -32,14 +54,20 @@ def get_lint(file):
                                             "F407",
                                             "F8",
                                             "F901",
-                                            "E999"]):
-                if "F403" in i:
+                                            "E999"]):"""
+        try:
+            matched_error = re.match(get_error, i)
+            if matched_error != None:
+                error_code = matched_error.group(0)
+                error_loc = re.match(get_loc, i)
+                if error_code == Errors.F403:
                     if not scan_import(i):
+                        error_msg = "{}: {}".format(error_loc, Errors.msg[error_code])
                         i = ":".join(i.split(":")[:3]) + ": F403 Package not installed or is unavailable."
                     else:
                         verified_packages.append(i.split("'")[1].split()[1])
                         continue
-                elif "F405" in i:
+                elif error_code == Errors.F405:
                     if any(pkg in verified_packages for pkg in i.split(":")[-1][1:].split(", ")):
                         continue
                     else:
