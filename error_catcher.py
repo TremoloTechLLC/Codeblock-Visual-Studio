759a1d2192de33ba1352f139ba97d6324efb9428
Kalyan Sriram <coder.kalyan@gmail.com>
2018-08-13 17:44:18 -0700

    Made some more changes to error_checker - no api changes.

--- error_catcher.py
+++ error_catcher.py
@@ -24,7 +24,7 @@ class Errors:
 get_error = re.compile("[FE]\d{1,3}")
 get_loc = re.compile("\w+.py:\d+:\d+:")
 
-def get_lint(file):
+def ret_lint(file):
     # Temporarily redirect stdout to the lintstdout variable to get flake8 output
     oldstdout = sys.stdout
     sys.stdout = lintstdout = StringIO()
@@ -39,25 +39,21 @@ def get_lint(file):
     # set lint variable to captured output
     lint = lintstdout.getvalue()
 
+    for issue in lint.split("\n"):
+        yield issue
+
+
+def get_lint(file_name):
+    print(file_name)
     errors_to_return = {}
     warnings_to_return = {}
     verified_packages = []
-    sys.path.append("/".join(file.split("/")[:-1]))
-    for i in lint.split("\n"):
-        """print(i)
+    # sys.path.append("/".join(file.split("/")[:-1]))
+    sys.path.append(file_name.rstrip("/"))
+    for i in ret_lint(file_name):
         try:
-            if any(error in i for error in ["F402",
-                                            "F403",
-                                            "F404",
-                                            "F405",
-                                            "F406",
-                                            "F407",
-                                            "F8",
-                                            "F901",
-                                            "E999"]):"""
-        try:
-            matched_error = re.match(get_error, i)
-            if matched_error != None:
+            matched_error = re.match(get_error, i)  # try to match the lint line with an error marker
+            if matched_error != None:  # is this lint an error?
                 error_code = matched_error.group(0)
                 error_loc = re.match(get_loc, i)
                 if error_code == Errors.F403:
@@ -74,7 +70,7 @@ def get_lint(file):
                         i = ":".join(i.split(":")[:3]) + ": F405 '" + i.split("'")[1] + "'" + "is undefined."
                 i = "E: " + i
                 errors_to_return[i] = int(i.split(":")[2])
-            else:
+            else:  # it must be a warning
                 i = "W: " + i
                 warnings_to_return[i] = int(i.split(":")[2])
         except IndexError:
@@ -97,4 +93,4 @@ def scan_import(line):
 
 
 if __name__ == "__main__":
-    print(get_lint("mainwindow_controller.py"))
+    print(get_lint("./mainwindow_controller.py"))
